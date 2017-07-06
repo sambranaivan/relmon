afb8f4e680d0648a64925e5d68c36e23ed484615
Albeto A. Del Yesso <aa_dy@hotmail.com>
2017-07-06 17:45:36 -0300

    En AnomaliaPico la funcion check se implementa con mas de 100 valores leidos

--- Monitor/src/main/java/AnomaliaPico.java
+++ Monitor/src/main/java/AnomaliaPico.java
@@ -4,9 +4,17 @@ import java.util.Calendar;
 
 public class AnomaliaPico extends Anomalia  {
 	
+	private int maximoValores;
+	
 	//Constructor
 	public AnomaliaPico(double p_data){
 		super(p_data);
+		this.maximoValores = 100; // para que check se implemente despues de 100 valores leidos
+	}
+	
+	//setters
+	public int getMaximoValores(){
+		return this.maximoValores;
 	}
 	
 	//verifica si la anomalia supera un valor de referencia
@@ -15,27 +23,32 @@ public class AnomaliaPico extends Anomalia  {
 		double desv = 0;
 		ResultadoAnomalia r = new ResultadoAnomalia();
 		
-		//get current date
-		Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        String strDate = sdf.format(cal.getTime());
-        
-        //for (double value : p_data){
-        for (int i = 0; i< p_data.size()-1;i++){	
-        	//acumulacion de valores
-        	desv = desv + p_data.get(i);
-        }
-        
-        //Saca promedio de los valores
-        desv = desv / p_data.size();
-        
-        //desvio en porcentaje positivo
-        desv = Math.abs(desv / p_data.get(p_data.size()-1) * 100);
-        
-        if(desv >= this.getRefValue()){
-            msg = "<log date="+ strDate +"><type>DESVIO</type><variable>"+p_nombreVariable+"</variable><ref_value>"+this.getRefValue() +"</ref_value><actual_value>"+p_data.get(p_data.size()-1)+"</actual_value><desv>"+desv+"<desv></log>";
-    		r.add(true, msg);
-        }
+		//solo se implementa cuando existen más de un maximo de valores leidos
+		if (p_data.size() > this.getMaximoValores()){
+			
+			//get current date
+			Calendar cal = Calendar.getInstance();
+	        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	        String strDate = sdf.format(cal.getTime());
+	        
+	        //for (double value : p_data){
+	        for (int i = 0; i< p_data.size()-1;i++){	
+	        	//acumulacion de valores
+	        	desv = desv + p_data.get(i);
+	        }
+	        
+	        //Saca promedio de los valores
+	        desv = desv / p_data.size();
+	        
+	        //desvio en porcentaje positivo
+	        desv = Math.abs(desv / p_data.get(p_data.size()-1) * 100);
+	        
+	        if(desv >= this.getRefValue()){
+	            msg = "<log date="+ strDate +"><type>DESVIO</type><variable>"+p_nombreVariable+"</variable><ref_value>"+this.getRefValue() +"</ref_value><actual_value>"+p_data.get(p_data.size()-1)+"</actual_value><desv>"+desv+"<desv></log>";
+	    		r.add(true, msg);
+	        }
+			
+		}
 
 		return r;
 	}
