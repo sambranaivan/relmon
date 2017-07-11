f92f7fdf6c355401b67436ce3602da150e62a727
Ivan Sambrana <sambranaivan@gmail.com>
2017-07-11 12:25:43 -0300

    delete api keys

--- Monitor/src/main/java/SmsSender.java
+++ Monitor/src/main/java/SmsSender.java
@@ -6,15 +6,15 @@ import com.twilio.type.PhoneNumber;
 
 public class SmsSender implements iObserver{
 	// Find your Account Sid and Token at twilio.com/user/account
-	  public static final String ACCOUNT_SID = "AC9a12d6d1b98743ad8b0539ff387d4fef";
-	  public static final String AUTH_TOKEN = "a50ad29c928220b2f5ac636194c08255";
+	  public static final String ACCOUNT_SID = "";
+	  public static final String AUTH_TOKEN = "";
 	@Override
 	public void update() {
 		
 		 Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-		    Message message = Message.creator(new PhoneNumber("+18569246781"),
-		        new PhoneNumber("+3794688483"), 
+		    Message message = Message.creator(new PhoneNumber("+"),
+		        new PhoneNumber("+"), 
 		        "This is the ship that made the Kessel Run in fourteen parsecs?").create();
 
 		    System.out.println(message.getSid());
